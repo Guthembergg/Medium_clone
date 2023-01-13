3c0bc2dec67b60dd135f67b3418eff48530a6b06
Karim Guettech <karimguettech@gmail.com>
2023-01-13 19:26:22 +0100

    added tablet resolution

---
+++ assets/Javascript/Medium.js
@@ -0,0 +1,13 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("started").style.backgroundColor = "#1a8917";
+    document.getElementById("nav").style.backgroundColor = "rgb(255,255,255";
+  } else {
+    document.getElementById("started").style.backgroundColor = "rgb(0,0,0)";
+    document.getElementById("nav").style.backgroundColor = "#ffc017";
+  }
+}
