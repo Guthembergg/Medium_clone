4b82f90890bd843f23ec2b03210c3f7da95bc1a9
Karim Guettech <karimguettech@gmail.com>
2023-01-15 19:08:36 +0100

    better media queries

--- Medium.js
+++ Medium.js
@@ -4,8 +4,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 310 ||
-    document.documentElement.scrollTop > 310
+    document.body.scrollTop > 460 ||
+    document.documentElement.scrollTop > 460
   ) {
     document.getElementById("started").style.backgroundColor = "#1a8917";
     document.getElementById("nav").style.backgroundColor = "rgb(255,255,255";
@@ -20,27 +20,18 @@ let arr = document.getElementsByTagName("path");
 var mElements = Array.from(arr);
 
 setInterval(() => {
-  // Choose a random number of M elements to hide
   let lengthTime = mElements.length / 2;
-  let lengthTime2 = mElements.length / 2;
 
   // Hide the chosen elements
-
   let num = Math.floor(Math.random() * lengthTime);
   mElements[num].style.opacity = "0";
 
-  // Choose a random interval of time to wait before showing the elements again
+  // Choose a random interval of time to wait
   let showInterval = Math.floor(Math.random() * 7000);
 
   setTimeout(() => {
     // Show the hidden elements again
-
-    let num2 = Math.floor(Math.random() * lengthTime2);
+    let num2 = Math.floor(Math.random() * lengthTime);
     mElements[num2].style.opacity = "1";
   }, showInterval);
-}, 610);
-
-// let firstG = document.getElementById("id_G");
-// var ArrG = Array.from(firstG);
-// ArrG.aria_label = "M M";
-console.log(mElements);
+}, 100);
